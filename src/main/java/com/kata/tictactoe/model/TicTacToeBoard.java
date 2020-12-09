bad984f30270bc9bcce30222d0a0de6bab620750
Tim Verte <tim.verte@ordina.be>
2020-12-09 10:06:48 +0100

    - Added Board Tests

--- src/main/java/com/kata/tictactoe/model/TicTacToeBoard.java
+++ src/main/java/com/kata/tictactoe/model/TicTacToeBoard.java
@@ -1,21 +1,72 @@
 package com.kata.tictactoe.model;
 
+import org.springframework.stereotype.Component;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class TicTacToeBoard {
 
     private List<String[]> gameBoardMatrix;
+    private final int maxNumberOfLines = 3;
+    private final int maxNumberOfColumns = 3;
 
-    public TicTacToeBoard(){
+    public TicTacToeBoard() {
         gameBoardMatrix = new ArrayList<>(3);
-        String[] emptyLine = {" ", " ", " "};
-        for (int i = 1; i<=3 ; i++)
+
+        for (int i = 0; i < maxNumberOfLines; i++){
+            String[] emptyLine = {" ", " ", " "};
             gameBoardMatrix.add(emptyLine);
+        }
     }
 
     public List<String[]> getGameBoardMatrix(){
         return gameBoardMatrix;
+    }
+
+
+    public int calculateLineOfPosition(int position){
+        return position <= 3 ? 1 : position <= 6 ? 2 : 3;
+    }
+
+    public String getValueOfPos(int position){
+        String valueOfPosition = null;
+        int lineOfPos = calculateLineOfPosition(position);
+        int indexOfListToGet = lineOfPos-1;
+        int indexOfPosition = position-1;
+
+        if (indexOfListToGet < 1){
+            valueOfPosition = gameBoardMatrix.get(indexOfListToGet)[indexOfPosition];
+        } else if (indexOfListToGet == 1){
+            indexOfPosition = indexOfPosition - 3;
+            valueOfPosition = gameBoardMatrix.get(indexOfListToGet)[indexOfPosition];
+        } else {
+            indexOfPosition = indexOfPosition - 6;
+            valueOfPosition = gameBoardMatrix.get(indexOfListToGet)[indexOfPosition];
+        }
+
+        return valueOfPosition;
+    }
+
+    public void addValueOnPosition(String value, int position){
+        int lineOfPos = calculateLineOfPosition(position);
+        int indexOfListToGet = lineOfPos-1;
+        int indexOfPosition = position-1;
+
+        String[] line = gameBoardMatrix.get(indexOfListToGet);
+
+        // TODO ==> Add check ? ==> positionAlreadyTaken
+
+        if (indexOfListToGet < 1){
+            line[indexOfPosition] = value;
+        } else if (indexOfListToGet == 1){
+            indexOfPosition = indexOfPosition - 3;
+            line[indexOfPosition] = value;
+        } else {
+            indexOfPosition = indexOfPosition - 6;
+            line[indexOfPosition] = value;
+        }
     }
 
 }
--- src/test/java/com/kata/tictactoe/model/TicTacToeBoardTest.java
+++ src/test/java/com/kata/tictactoe/model/TicTacToeBoardTest.java
@@ -2,20 +2,55 @@ package com.kata.tictactoe.model;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TicTacToeBoardTest {
 
     @Test
-    public void shouldMakeANewEmptyGameBoard(){
-
+    public void shouldMakeANewEmptyGameBoard() {
         TicTacToeBoard gameBoard = new TicTacToeBoard();
 
-        List<String[]> board = gameBoard.getGameBoardMatrix();
+        assertNotNull(gameBoard);
+        assertEquals(" ", gameBoard.getValueOfPos(1));
+        assertEquals(" ", gameBoard.getValueOfPos(4));
+        assertEquals(" ", gameBoard.getValueOfPos(8));
+    }
 
-        assertNotNull(board);
+    @Test
+    public void shouldGiveTheCorrectLineInTheBoardmatrixOfTheGivenPosition() {
+        TicTacToeBoard gameBoard = new TicTacToeBoard();
+
+        int positionToCheck = 9;
+        assertEquals(3, gameBoard.calculateLineOfPosition(positionToCheck));
+
+        positionToCheck = 4;
+        assertEquals(2, gameBoard.calculateLineOfPosition(positionToCheck));
+
+        positionToCheck = 2;
+        assertEquals(1, gameBoard.calculateLineOfPosition(positionToCheck));
+    }
+
+    @Test
+    public void shouldAddAValueToTheBoardOnGivenPosition() {
+        TicTacToeBoard emptyBoard = new TicTacToeBoard();
+        String xValue = "X";
+        String oValue = "O";
+
+        emptyBoard.addValueOnPosition(xValue, 5);
+        emptyBoard.addValueOnPosition(oValue, 9);
+
+        assertEquals("X", emptyBoard.getValueOfPos(5));
+        assertEquals("O", emptyBoard.getValueOfPos(9));
+    }
+
+    @Test
+    public void shouldGetAValueOfAGivenPosition() {
+        TicTacToeBoard emptyBoard = new TicTacToeBoard();
+        String xValue = "X";
+
+        assertEquals(" ", emptyBoard.getValueOfPos(5));
+        emptyBoard.addValueOnPosition(xValue, 5);
+        assertEquals("X", emptyBoard.getValueOfPos(5));
     }
 
 }
