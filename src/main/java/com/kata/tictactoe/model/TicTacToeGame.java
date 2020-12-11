b0a6796ca94da33bea2cc78355229dd3626d6d31
Tim Verte <tim.verte@ordina.be>
2020-12-11 13:48:08 +0100

    - add implementation to Game

--- src/main/java/com/kata/tictactoe/model/TicTacToeGame.java
+++ src/main/java/com/kata/tictactoe/model/TicTacToeGame.java
@@ -1,26 +1,31 @@
 package com.kata.tictactoe.model;
 
+import com.kata.tictactoe.utils.WinnerStatus;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class TicTacToeGame {
+public class TicTacToeGame implements Game {
 
-    private final int maxNumberOfPlayers = 2;
+    private final int numberOfPlayers = 2;
+
     private List<TicTacToePlayer> playerList = new ArrayList<>(2);
+    private TicTacToeBoard gameBoard;
 
 
     public TicTacToeGame(){
-
+        this.gameBoard = new TicTacToeBoard();
     }
 
-
     public void addPlayers(List<TicTacToePlayer> playersToAdd) {
+        if(playersToAdd.size() > numberOfPlayers)
+            throw new IllegalArgumentException("Trying to add too much players to the game");
         for(TicTacToePlayer player : playersToAdd)
             playerList.add(player);
     }
 
     public int getNumberOfPlayers(){
-        return playerList.size();
+        return numberOfPlayers;
     }
 
 }
