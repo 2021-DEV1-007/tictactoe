b1ce4906e08eaa78ed7fbf5e2257a723766a8343
Tim Verte <tim.verte@ordina.be>
2020-12-11 12:59:52 +0100

    - Change implementation so test succeeds

--- src/main/java/com/kata/tictactoe/model/TicTacToePlayer.java
+++ src/main/java/com/kata/tictactoe/model/TicTacToePlayer.java
@@ -7,7 +7,17 @@ public class TicTacToePlayer implements Player {
     private String symbol;
 
     public TicTacToePlayer(int id) {
+        if (id > 1)
+            throw new IllegalArgumentException("Id of players should be smaller than 2");
+
         this.id = id;
+        if (id == 0) {
+            this.name = "Player-X";
+            this.symbol = "X";
+        } else {
+            this.name = "Player-O";
+            this.symbol = "O";
+        }
     }
 
     public int getId() {
