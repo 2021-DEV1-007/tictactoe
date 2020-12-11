f5198ccfcfd971fca10f99c7096b013f614c0822
Tim Verte <tim.verte@ordina.be>
2020-12-11 13:18:19 +0100

    - add implementation for Board Tests

--- src/main/java/com/kata/tictactoe/model/TicTacToeBoard.java
+++ src/main/java/com/kata/tictactoe/model/TicTacToeBoard.java
@@ -1,7 +1,5 @@
 package com.kata.tictactoe.model;
 
-import org.springframework.stereotype.Component;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -14,7 +12,6 @@ public class TicTacToeBoard {
 
     public TicTacToeBoard() {
         gameBoardMatrix = new ArrayList<>(3);
-
         for (int i = 0; i < maxNumberOfLines; i++){
             String[] emptyLine = {" ", " ", " "};
             gameBoardMatrix.add(emptyLine);
@@ -25,12 +22,13 @@ public class TicTacToeBoard {
         return gameBoardMatrix;
     }
 
-
-    public int calculateLineOfPosition(int position){
+    public int calculateLineOfPosition(int position) {
+        if (position < 0 || position > 9)
+            throw new IllegalArgumentException("The given position has to be between 1 and 9");
         return position <= 3 ? 1 : position <= 6 ? 2 : 3;
     }
 
-    public String getValueOfPos(int position){
+    public String getValueOfPos(int position) {
         String valueOfPosition = null;
         int lineOfPos = calculateLineOfPosition(position);
         int indexOfListToGet = lineOfPos-1;
@@ -45,18 +43,18 @@ public class TicTacToeBoard {
             indexOfPosition = indexOfPosition - 6;
             valueOfPosition = gameBoardMatrix.get(indexOfListToGet)[indexOfPosition];
         }
-
         return valueOfPosition;
     }
 
-    public void addValueOnPosition(String value, int position){
+    public void addValueOnPosition(String value, int position) {
+        if (!((value.equals("X")) | (value.equals("O"))))
+           throw new IllegalArgumentException("Something went wrong, wrong player symbol is being added to the board");
+
         int lineOfPos = calculateLineOfPosition(position);
         int indexOfListToGet = lineOfPos-1;
         int indexOfPosition = position-1;
 
         String[] line = gameBoardMatrix.get(indexOfListToGet);
-
-        // TODO ==> Add check ? ==> positionAlreadyTaken
 
         if (indexOfListToGet < 1){
             line[indexOfPosition] = value;
