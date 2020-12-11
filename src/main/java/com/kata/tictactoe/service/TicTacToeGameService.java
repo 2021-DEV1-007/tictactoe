c48edaf0067ec433c7dad27564440bfc2e5889bd
Tim Verte <tim.verte@ordina.be>
2020-12-11 14:10:46 +0100

    - add implementation for Game Service

--- src/main/java/com/kata/tictactoe/service/TicTacToeGameService.java
+++ src/main/java/com/kata/tictactoe/service/TicTacToeGameService.java
@@ -1,25 +1,49 @@
 package com.kata.tictactoe.service;
 
+import com.kata.tictactoe.model.TicTacToeGame;
 import com.kata.tictactoe.model.TicTacToePlayer;
+import com.kata.tictactoe.utils.ScannerUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class TicTacToeGameService {
+@Service
+public class TicTacToeGameService implements GameService {
 
     private List<TicTacToePlayer> playerList;
+    private TicTacToeGame game;
 
-    public TicTacToeGameService(){
+    @Autowired
+    private ScannerUtil scan;
 
+    @Override
+    public void setUpGame(){
+        game = new TicTacToeGame();
+        try {
+            createPlayers(game.getNumberOfPlayers());
+            addPlayersToGame();
+        } catch (IllegalArgumentException ie){
+            scan.sayToPlayer(ie.getMessage());
+        } catch (NullPointerException np){
+            scan.sayToPlayer("There seems to be a problem while setting up the game, playerList is empty");
+        }
     }
 
+    @Override
+    public void addPlayersToGame(){
+        game.addPlayers(playerList);
+    }
+
+    @Override
     public void createPlayers(int numberToCreate) {
         TicTacToePlayer createdPlayer;
         playerList = new ArrayList<>(numberToCreate);
         int index = 0;
 
         while(index < numberToCreate){
-            createdPlayer = new TicTacToePlayer();
+            createdPlayer = new TicTacToePlayer(index);
             playerList.add(createdPlayer);
             index++;
         }
