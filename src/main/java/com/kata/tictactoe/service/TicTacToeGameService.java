23d8ba3a31b8b42e884e974e5a392799fd2c1a48
Tim Verte <tim.verte@ordina.be>
2020-12-11 14:12:09 +0100

    - add more implemantation for Game Service

--- src/main/java/com/kata/tictactoe/service/TicTacToeGameService.java
+++ src/main/java/com/kata/tictactoe/service/TicTacToeGameService.java
@@ -31,6 +31,55 @@ public class TicTacToeGameService implements GameService {
         }
     }
 
+    @Override
+    public void startGame() {
+        try {
+            showRules();
+            boolean doAnotherTurn = true;
+            String lastPlayer = "";
+
+            while (doAnotherTurn) {
+                for (TicTacToePlayer player : playerList) {
+                    lastPlayer = player.getName();
+                    playerDoesATurn(player);
+                    if (game.winnerFoundOrBoardFull()) {
+                        doAnotherTurn = false;
+                        break;
+                    }
+                }
+            }
+            scan.sayToPlayer(game.declareResult(lastPlayer));
+        } catch (IllegalArgumentException ie){
+            scan.sayToPlayer("Cannot declare the winner when game has not finished");
+        } catch (Exception e) {
+            scan.sayToPlayer("The game will end. Server error: CheckDiagonalWinner:" + e.getMessage());
+        }
+    }
+
+    public void playerDoesATurn(TicTacToePlayer player) {
+        boolean askForChoice = true;
+        int playersChoice = -1;
+
+        while (askForChoice){
+            try {
+                scan.sayToPlayer("\n" + player.getName() + ", what position on the board do you choose?");
+                playersChoice = Integer.parseInt(scan.getInputFromUser());
+
+                if (game.isChosenPositionValid(playersChoice)) {
+                    game.addValueOnPosition(player.getSymbol(), playersChoice);
+                    askForChoice = false;
+                } else {
+                    scan.sayToPlayer("You chose an invalid square, try again");
+                }
+            } catch (NumberFormatException ne) {
+                scan.sayToPlayer("Don't use text, try again?");
+            } catch (IllegalArgumentException ie){
+                scan.sayToPlayer("Your position has to be between 1 and 9, try again");
+            }
+        }
+        displayTheBoard();
+    }
+
     public void displayTheBoard(){
         scan.sayToPlayer("\n-------------");
         for(String[] line: game.getBoard()) {
